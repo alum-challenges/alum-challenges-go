84c8ef8cf378fa79a448014e5bb0b19cfe1bf540
Dana-f559 <bogdanafarace559@gmail.com>
2024-04-25 22:17:48 +0300

    templates changed

--- main.go
+++ main.go
@@ -9,47 +9,47 @@ import (
 	"github.com/russross/blackfriday"
 )
 
-type Hehe struct {
+type Stored_Html struct {
 	Html_data template.HTML
 }
 
 func main() {
 	r := mux.NewRouter()
 
-	tmpl := template.Must(template.ParseFiles("static/layout.html"))
-	tmpl2 := template.Must(template.ParseFiles("static/index.html"))
+	tmpl := template.Must(template.ParseGlob("static/*.html"))
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl2.ExecuteTemplate(w, "index.html", nil)
+		tmpl.ExecuteTemplate(w, "index.html", nil)
 	})
 
 	r.HandleFunc("/python/{week}/{filename}", func (w http.ResponseWriter, r *http.Request) {
 		// get the weeks name from url
-        vars := mux.Vars(r)
-        week := vars["week"]
+		vars := mux.Vars(r)
+		week := vars["week"]
 		file_name := vars["filename"]
 		
 		a := fmt.Sprintf("python/week-%s/%s.md", week, file_name)
-
+	
 		data, err := ioutil.ReadFile(a)
-
+	
 		if err != nil {
 			fmt.Println("Couldnt open the file")
 			//! add to quit or something
 		} 
 			// translate the file into markdown
 		markdown := blackfriday.MarkdownCommon(data)
-
+	
 		x := template.HTML(string(markdown))
-
+	
 		// add the data to the slice
-		Datas := Hehe {
+		Datas := Stored_Html {
 			Html_data: x,
 		}
-
+	
 		// render the template
-		tmpl.Execute(w, Datas)	
-    })
+		tmpl.ExecuteTemplate(w, "layout.html", Datas)
+		// tmpl.Execute(w, Datas)	
+	})
 
     http.ListenAndServe(":8000", r)
 }
